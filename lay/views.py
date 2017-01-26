f9917bdf2e68dee9d9ec0d0dcc60b79d19d1c90c
njdaniel <nicholasjdaniel@gmail.com>
2017-01-26 16:40:45 -0500

    fixed hier problem with templates

--- lay/views.py
+++ lay/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Put shit here")
+    return render(request, 'lay/index.html', {})
